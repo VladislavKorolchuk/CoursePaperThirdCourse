7223e39720bae77d055d885a70a56bbbaf481805
93294 <9329496@mail.ru>
2022-11-27 00:16:49 +0300

    Initial commit

---
+++ src/main/java/com/coursepaper/coursepaperthirdcourse/CoursePaperThirdCourseApplication.java
@@ -0,0 +1,13 @@
+package com.coursepaper.coursepaperthirdcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePaperThirdCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursePaperThirdCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coursepaper/coursepaperthirdcourse/CoursePaperThirdCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursepaper.coursepaperthirdcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePaperThirdCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
